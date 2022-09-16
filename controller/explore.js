6dea169aa2c937df4dc8f4c3875c5801621548b7
namit7080 <81043272+namit7080@users.noreply.github.com>
2022-09-16 22:45:57 +0530

    Update explore.js

--- controller/explore.js
+++ controller/explore.js
@@ -4,6 +4,7 @@ const Post= require('../models/PostContainer');
 
 module.exports.Explore= async function(req,res){
 
+    console.log("get");
     let post= await Post.find({}).sort({'vote':-1});
 
     return res.status(200).json({"message":post})
